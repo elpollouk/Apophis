73ac52b9bf423399ed6374dc878ab44c001f8cc0
Adrian O'Grady <elpollouk@users.noreply.github.com>
2019-05-19 16:27:59 +0100

    Added an E2E test for backpropagation training.

---
+++ Apophis.Tests/Training/Algorithms/BackPropTests.cpp
@@ -0,0 +1,53 @@
+#include "stdafx.h"
+#include "apophis/apophis.h"
+
+using namespace Microsoft::VisualStudio::CppUnitTestFramework;
+using namespace Apophis;
+using namespace Apophis::TransferFunction;
+using namespace Apophis::Training;
+
+namespace ApophisTests { namespace Training { namespace Algorithms {
+
+	Example CreateExample(real i0, real i1, real o0)
+	{
+		real input[] = { i0, i1 };
+		real output[] = { o0 };
+		return Example(input, 2, output, 1);
+	}
+
+	TEST_CLASS(BackPropTests)
+	{
+	public:
+		TEST_METHOD(Xor)
+		{
+			const auto TRAINING_ITERATIONS = 5000;
+			const auto TRAINING_ERROR = 0.00001f;
+			const auto LEARNING_RATE = 0.05f;
+			const auto MOMENTUM = 0.5f;
+
+			ExampleSet trainingSet(2, 1);
+			trainingSet.AddExample(CreateExample(0.f, 0.f, 0.f));
+			trainingSet.AddExample(CreateExample(1.f, 0.f, 1.f));
+			trainingSet.AddExample(CreateExample(0.f, 1.f, 1.f));
+			trainingSet.AddExample(CreateExample(1.f, 1.f, 0.f));
+
+			BackPropNetwork network(trainingSet.InputSize, LEARNING_RATE, MOMENTUM);
+			network.AddLayer<Relu>(3);
+			network.AddLayer<Relu>(1);
+
+			Evaluator evaluator(network, Loss::SquaredError);
+			StoppingCondition::AnyStoppingCondition stoppingConditions;
+			stoppingConditions.Add<StoppingCondition::MetricLessThan<real>>(Data::METRIC_TRAINING_ERROR, TRAINING_ERROR);
+			stoppingConditions.Add<StoppingCondition::NumTrainingIterations>(TRAINING_ITERATIONS);
+
+			Trainer trainer(network, evaluator);
+			trainer.Run(trainingSet, stoppingConditions);
+
+			Assert::IsTrue(network.Calculate(trainingSet[0].Input)[0] <= 0.01f);
+			Assert::IsTrue(network.Calculate(trainingSet[1].Input)[0] >= 0.99f);
+			Assert::IsTrue(network.Calculate(trainingSet[2].Input)[0] >= 0.99f);
+			Assert::IsTrue(network.Calculate(trainingSet[3].Input)[0] <= 0.01f);
+		}
+	};
+
+}}}
